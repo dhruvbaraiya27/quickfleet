38c9533820c6ccb2e6371f62885c7ba5a9c9d822
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-05-28 19:28:48 -0400

    created Repository interface Role Repository

---
+++ src/main/java/com/quickfleet/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.quickfleet.repository;
+
+import com.quickfleet.model.Role;
+import com.quickfleet.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<User> findByName(String name);
+}
