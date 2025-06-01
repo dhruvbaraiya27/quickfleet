37fcef65c7512e56cc005de458299d9d084cb04c
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-05-31 23:13:29 -0500

    created Auth Controller for Login and Register

---
+++ src/main/java/com/quickfleet/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.quickfleet.controller;
+
+import com.quickfleet.dto.AuthResponse;
+import com.quickfleet.dto.LoginRequest;
+import com.quickfleet.dto.RegisterRequest;
+import com.quickfleet.model.User;
+import com.quickfleet.service.AuthService;
+import com.quickfleet.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
+        User user = userService.register(request);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
