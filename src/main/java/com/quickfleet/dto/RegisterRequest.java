f2aba5b8ba0212cdfd0c55c40c98c87620851359
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-05-31 23:02:13 -0500

    updated Register Request

--- src/main/java/com/quickfleet/dto/RegisterRequest.java
+++ src/main/java/com/quickfleet/dto/RegisterRequest.java
@@ -1,8 +1,12 @@
 package com.quickfleet.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequest {
     private String name;
     private String email;
