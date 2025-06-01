0d63a1c1d8799f94f23b0bd229fc5d8ef00ccc4c
dhruvbaraiya27 <baraiya.d@northeastern.edu>
2025-06-01 00:29:44 -0500

    Move JWT Util to different package

---
+++ src/main/java/com/quickfleet/config/JwtUtil.java
@@ -0,0 +1,46 @@
+package com.quickfleet.config;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import static javax.crypto.Cipher.SECRET_KEY;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey key = Keys.hmacShaKeyFor("mysecretkey1234567890mysecretkey1234567890".getBytes(StandardCharsets.UTF_8));
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hrs
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            extractClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Claims extractClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
